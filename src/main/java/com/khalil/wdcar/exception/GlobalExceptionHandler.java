d5a24a0c2141da886e79397544c4f45eb56b5701
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-10-24 21:52:25 +0100

    add new exception

--- src/main/java/com/khalil/wdcar/exception/GlobalExceptionHandler.java
+++ src/main/java/com/khalil/wdcar/exception/GlobalExceptionHandler.java
@@ -1,2 +1,16 @@
-package com.khalil.wdcar.exception;public class GlobalExceptionHandler {
+package com.khalil.wdcar.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(InvalidInputException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleMatriculationExistsException(InvalidInputException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
 }
