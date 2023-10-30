8762748fecb25f13b6fa6cb7aea2c866536c11d0
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-10-30 15:33:43 +0100

    Add new query

--- src/main/java/com/khalil/wdcar/repository/CarRepository.java
+++ src/main/java/com/khalil/wdcar/repository/CarRepository.java
@@ -1,10 +1,13 @@
 package com.khalil.wdcar.repository;
 
+import com.khalil.wdcar.beans.CarRentalBean;
 import com.khalil.wdcar.entity.Car;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +16,7 @@ import java.util.List;
 public interface CarRepository extends JpaRepository<Car, Long> {
     Page<Car> findAll(Specification spec, Pageable pageable);
     List<Car> findAll(Specification spec);
+    @Query(value = "select new com.khalil.wdcar.beans.CarRentalBean(c.name, c.brande, c.style) from Car c where lower(c.name) like lower(concat('%',:label,'%')) or lower(c.brande) like lower(concat('%',:label,'%')) or lower(c.style) like lower(concat('%',:label,'%'))")
+    List<CarRentalBean> getSearch(@Param("label") String label);
+
 }
--- src/main/java/com/khalil/wdcar/repository/ClientRepository.java
+++ src/main/java/com/khalil/wdcar/repository/ClientRepository.java
@@ -1,13 +1,28 @@
 package com.khalil.wdcar.repository;
 
+import com.khalil.wdcar.beans.CarRentalBean;
 import com.khalil.wdcar.entity.Client;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
     Page<Client> findAll(Specification spec, Pageable pageable);
+    @Query(value = "select new com.khalil.wdcar.beans.CarRentalBean(c.adress, c.cin, c.city, c.country, c.firstName, c.lastName, c.licence, c.userName) from Client c " +
+            "where lower(c.adress) like lower(concat('%',:label,'%')) or " +
+            "lower(c.cin) like lower(concat('%',:label,'%')) or " +
+            "lower(c.city) like lower(concat('%',:label,'%')) or " +
+            "lower(c.country) like lower(concat('%',:label,'%')) or " +
+            "lower(c.firstName) like lower(concat('%',:label,'%')) or " +
+            "lower(c.lastName) like lower(concat('%',:label,'%')) or " +
+            "lower(c.licence) like lower(concat('%',:label,'%')) or " +
+            "lower(c.userName) like lower(concat('%',:label,'%'))")
+    List<CarRentalBean> getSearch(@Param("label") String label);
 }
