ffc5d52c0297f42ad524d209d5327d8665ba2212
khalilfarouqi <bio.khalil.f@gmail.com>
2023-11-11 15:28:38 +0100

    set lase check in

--- src/main/java/com/khalil/wdcar/service/UserService.java
+++ src/main/java/com/khalil/wdcar/service/UserService.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -30,6 +31,7 @@ public class UserService implements IBaseService<User, UserDto> {
     @Override
     @Transactional
     public UserDto save(UserDto userDto) {
+        userDto.setLastCheckIn(new Date());
         return modelMapper.map(userRepository.save(modelMapper.map(userDto, User.class)), UserDto.class);
     }
 
