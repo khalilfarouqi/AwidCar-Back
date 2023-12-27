eb2e6811a7023b893b423a9ed55e104bfdd0ecaa
khalilfarouqi <bio.khalil.f@gmail.com>
2023-12-27 14:17:28 +0100

    Add keyloack

--- src/main/java/com/khalil/wdcar/configs/SecurityConfig.java
+++ src/main/java/com/khalil/wdcar/configs/SecurityConfig.java
@@ -1,23 +1,35 @@
 package com.khalil.wdcar.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+    @Value("${keycloak.auth-server-url}")
+    private String jwkSetUri;
+
+    @Bean
+    public ReactiveJwtDecoder reactiveJwtDecoder() {
+        return NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri).build();
+    }
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
         return serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**")
                         .permitAll()
                         .anyExchange().authenticated()
-                ).oauth2ResourceServer((oauth) -> oauth
-                        .jwt(Customizer.withDefaults()))
+                )
+                .oauth2ResourceServer((oauth) -> oauth
+                        .jwt(jwt -> jwt.jwtDecoder(reactiveJwtDecoder()))
+                )
                 .build();
     }
 }
